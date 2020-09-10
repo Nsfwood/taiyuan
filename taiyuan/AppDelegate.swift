b47d863c38dffea52be72b2f1e11c61ddf479587
Alexander <a.rohrig@me.com>
2020-09-10 12:10:12 -0700

    Initial Commit

---
+++ taiyuan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  taiyuan
+//
+//  Created by Alexander Rohrig on 9/10/20.
+//  Copyright © 2020 Alexander Rohrig. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ taiyuan/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  taiyuan
+//
+//  Created by Alexander Rohrig on 9/10/20.
+//  Copyright © 2020 Alexander Rohrig. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
