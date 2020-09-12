701a070c90d7b26998fbf0c0d29486e6492c3452
Alexander <me@alexanderrohrig.com>
2020-09-12 12:16:25 -0700

    Added open on dock click
    and Touchbar setup

--- taiyuan/AppDelegate.swift
+++ taiyuan/AppDelegate.swift
@@ -10,16 +10,41 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    lazy var windows = NSWindow()
 
-
+    @IBAction func addNewPalettePressed(_ sender: Any) {
+        let newList = NSColorList(name: "New Palette")
+        newList.insertColor(NSColor.white, key: "New Color", at: 0)
+        do {
+            try newList.write(to: URL(fileURLWithPath: "newpalette.clr"))
+        }
+        catch {
+            print("color list failed to save")
+        }
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        NSApplication.shared.isAutomaticCustomizeTouchBarMenuItemEnabled = true
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
+    
+    func applicationDockMenu(_ sender: NSApplication) -> NSMenu? {
+        let menu = NSMenu(title: "Open Color Picker")
+//        let item = NSMenuItem(title: "Open Color Picker", action: <#T##Selector?#>, keyEquivalent: "+")
+        return menu
+    }
+    
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        if !flag {
+            for window in sender.windows {
+                window.makeKeyAndOrderFront(self)
+            }
+        }
+        return true
+    }
 
 }
 
